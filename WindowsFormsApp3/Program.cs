46c9d5fc0c6d29b0d0487b2f89d290d953a0ef8e
Intrus1ve <98790607+Intrus1ve@users.noreply.github.com>
2023-01-28 01:16:24 +0300

    Regex Searcher Document File

---
+++ WindowsFormsApp3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string filePath = "C:\\Users\\Intrus1ve\\Desktop\\a.txt";
+        string searchPattern = @"";
+        string outputFile = "C:\\Users\\Intrus1ve\\Desktop\\b.txt";
+
+        string text = File.ReadAllText(filePath);
+        MatchCollection matches = Regex.Matches(text, searchPattern);
+
+        using (StreamWriter writer = new StreamWriter(outputFile))
+        {
+            foreach (Match match in matches)
+            {
+                writer.WriteLine(match.Value);
+            }
+        }
+    }
+}
